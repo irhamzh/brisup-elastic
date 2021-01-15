543fc3e3960590e1524753dd5a8615b38fd65de5
임나연 <ammar.fiky@gmail.com>
2021-01-15 16:14:05 +0700

    feat: :sparkles: user,role,rooms,areas

--- src/references.ts
+++ src/references.ts
@@ -1,11 +1,52 @@
 import { Reference } from "./types";
+import firestoreTimeStampToDate from "./util/firestoreTimeStampToDate";
 
 // Records should be added here to be indexed / made searchable
+const MasterData: Array<Reference> = [
+  {
+    collection: "users",
+    index: "bri_corpu_users",
+    transform: (data, parent) => firestoreTimeStampToDate(data),
+    // include: [
+    //   "name",
+    //   "email",
+    //   "role",
+    //   "division",
+    //   "profilePicture",
+    //   "createdAt",
+    //   "updatedAt",
+    // ],
+  },
+  {
+    collection: "roles",
+    index: "bri_corpu_roles",
+    transform: (data, parent) => firestoreTimeStampToDate(data),
+    // include: [
+    //   "name",
+    //   "fixedAsset",
+    //   "procurement",
+    //   "generalAffair",
+    //   "financialAdmin",
+    //   "masterData",
+    //   "createdAt",
+    //   "updatedAt",
+    // ],
+  },
+  {
+    collection: "ruangans",
+    index: "bri_corpu_rooms",
+    transform: (data, parent) => firestoreTimeStampToDate(data),
+    // include: ["name", "createdAt", "updatedAt"],
+  },
+];
+
 const references: Array<Reference> = [
+  ...MasterData,
   {
     collection: "areas",
     index: "bri_corpu_areas",
-    include: ["name", "updatedAt", "createdAt"],
+    transform: (data, parent) => firestoreTimeStampToDate(data),
+    // include: ["name", "updatedAt", "createdAt"],
   },
 ];
 
